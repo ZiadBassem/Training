aab557bac87469873be00c724f8025388f22277d
ZiadBassem <ziadbassem2001@gmail.com>
2025-06-20 17:18:57 +0300

    Update Training1.R

--- Training1.R
+++ Training1.R
@@ -8,11 +8,11 @@ head(mtcars, 5)
 library(ggplot2)
 #create a Scatter Plot of miles per gallon (mpg) and displacement (disp)
 ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle('displacement vs miles per gallon')+labs(x='displacement',y= ('Milers per Gallon'))
+#Assign V-S Shaped Engines as Factors
 mtcars$vs<-as.character(mtcars$vs)
+#Create Colored Box Plot
 ggplot(aes(x=vs,y=mpg,fill = vs),data=mtcars)+geom_boxplot(alpha=0.3)+theme(legend.position = 'none')
-ggplot(aes(x=wt),data = mtcars)+geom_histogram(binwidth = bw)
-mtcars$wt
-data.frame(mtcars$wt)
-IQR(mtcars$vs)
-hist(mtcars$wt, breaks="FD")
+#Defining bw according to Freedman-Diaconis Rule
 bw <- 2 * IQR(mtcars$wt) / length(mtcars$wt)^(1/3)
+#Create Histogram based on wt
+ggplot(aes(x=wt),data = mtcars)+geom_histogram(binwidth = bw)
